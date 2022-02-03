2d11e36576938e808a0bc1bdd7432bca97f8a2cd
ClaudiaMendezA <qlaudiia.mendez@gmail.com>
2022-02-02 23:21:19 -0600

    catalogo historial de cursos

---
+++ backend/src/api/course-history/controllers/course-history.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  course-history controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::course-history.course-history');
---
+++ backend/src/api/course-history/routes/course-history.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * course-history router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::course-history.course-history');
