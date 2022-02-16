86e94002c877fcbc5b9369d01fd8ef872936807a
ClaudiaMendezA <qlaudiia.mendez@gmail.com>
2022-02-15 23:18:55 -0600

    agrega roles de usuario

---
+++ backend/src/api/customer-role/controllers/customer-role.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  customer-role controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::customer-role.customer-role');
---
+++ backend/src/api/customer-role/services/customer-role.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * customer-role service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::customer-role.customer-role');
