907ef3e8782e96f94f2d0ba7d48d743ea24628f0
ClaudiaMendezA <qlaudiia.mendez@gmail.com>
2022-03-06 20:52:53 -0600

    corrige bd cursos categoria y chapters

--- backend/src/api/course-category/controllers/course-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  course-category controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::course-category.course-category');
--- backend/src/api/course-category/services/course-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * course-category service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::course-category.course-category');
